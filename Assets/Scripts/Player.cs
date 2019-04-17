ed0ae2eb67d900529b84c36adec92e1470e3f151
Cherise <cherisetan@live.com>
2019-04-17 16:15:27 +1200

    Add ladders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,58 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
+    // Config params
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float climbSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+
+        gravityScaleAtStart = myRigidBody.gravityScale;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         Run();
+        climbRope();
+        Jump();
         flipSprite();
-
-        
     }
 
     private void Run()
     {
-        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
-        // this says to get the axis (it is the horizontal axis)
-
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); 
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
-        // creates a new vector2, made up of the controlthrow (i.e. x axis), and setting the y axis to be whatever the current velocity is
+        myRigidBody.velocity = playerVelocity;
 
-        myRigidBody.velocity = playerVelocity; //then sets the player rigidbody velocity to be this new velocity
-
-
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
+
+    private void climbRope()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        myRigidBody.gravityScale = 0f;
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        } 
+    }
+
+
     private void flipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        // when the absolute value of the movement is greater than 0 (i.e. if there is any movement at all)
+
         if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1);
-            //sets the local scale to be a new vector 2: the x will be either -1 or +1 (cos of Mathf.Sign)
-            // Keep y axis the same (1 -> want to keep it the same scale)
-
-        }
-        
-        
-        //if player moving horizontally
-        {
-            //reverse current scaling of x axis
         }
     }
 
 
-    //Mathf.abs returns the absolute value (if.e. converts a negative to a positive number)
-        //Mathf.sign - if float is positive it will return 1, if it is negative it will return -1
+
 }
 
